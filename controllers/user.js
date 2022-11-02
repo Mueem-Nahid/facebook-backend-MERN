0e1023ee54acab5f139ec87a89ced29f33c4c2e7
Mueem-Nahid <mueem51@gmail.com>
2022-11-02 22:59:25 +0600

    # Fix - success message removed from login

--- controllers/user.js
+++ controllers/user.js
@@ -109,7 +109,6 @@ exports.login = async (req, res) => {
          last_name: user.last_name,
          token: token,
          verified: user.verified,
-         message: 'Login successful.'
       })
    } catch (error) {
       res.status(500).json({ message: error.message });
