32d25dd385b4384c2f2cd79a81c7c819e1efe349
Mueem-Nahid <mueem51@gmail.com>
2023-03-12 23:28:44 +0600

    # Info - code refactored

--- middlwares/auth.js
+++ middlwares/auth.js
@@ -1,18 +1,19 @@
 const jwt = require("jsonwebtoken");
+const {sendResponse} = require("../helpers/utils");
 
 exports.authUser = async (req, res, next) => {
    try {
       let tmp = req.header("Authorization");
       const token = tmp ? tmp.slice(7, tmp.length) : ""; // extracting token without 'Bearer'
       if (!token)
-         return res.status(400).json({message: "Invalid authentification."});
+         return sendResponse(res, 400, 'Invalid authentification.');
       jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
          if (err)
-            return res.status(400).json({message: "Invalid authentification."});
+            return sendResponse(res, 400, 'Invalid authentification.');
          req.user = user;
          next();
       })
    } catch (error) {
-      return res.status(500).json({message: error.message});
+      return sendResponse(res, 500, error.message);
    }
 };
