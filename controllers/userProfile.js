a72f522e4b7d9b35ade763f12907e05426884ea0
Mueem-Nahid <mueem51@gmail.com>
2023-04-07 23:59:09 +0600

    # Info - get user's posts

--- controllers/userProfile.js
+++ controllers/userProfile.js
@@ -1,14 +1,15 @@
 const {sendResponse} = require("../helpers/utils");
 const User = require('../models/User');
+const Post = require('../models/Post');
 
 exports.getProfile = async (req, res) => {
    try {
       const {username} = req.params;
       const userProfile = await User.findOne({username}).select("-password");
-      if (userProfile)
-         return sendResponse(res, 200, "User found.", userProfile);
-      else
+      if (!userProfile)
          return sendResponse(res, 404, "User not found.");
+      const userPosts = await Post.find({user: userProfile._id}).populate("user");
+      sendResponse(res, 200, "User found.", {userProfile, userPosts});
    } catch (error) {
       return sendResponse(res, 500, error.message);
    }
